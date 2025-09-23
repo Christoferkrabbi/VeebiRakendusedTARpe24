06b214996de887bb71594501a488fdecfd689656
christoferkrabbi <christoferkrabbi@gmail.com>
2025-09-23 14:51:19 +0300

    vormid ülesanded

---
+++ Veebirakendused/Vormid/lugemineScript.js
@@ -0,0 +1,11 @@
+function tervitus(){
+    let vastus = document.getElementById("vastus");
+    let nimi=document.getElementById("nimi");
+    let aastat=document.getElementById("aastat");
+
+    vastus.innerHTML="Tere tulemast, "+nimi.value+ "! Vasta küsimustele :)";
+    //vastus.innerHTML="Tere tulemas ja vasta küsimustele" +nimi.value;
+    "Sa õppisid "+aastat.value+" aastat";
+
+
+}
