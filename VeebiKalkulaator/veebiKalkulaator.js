bfb85f0146d1f0e4c20ffdce381eb05b20a74d48
ChristoferKrabbi <Christoferkrabbi@gamil.com>
2025-12-19 10:49:48 +0200

    calc is working

--- VeebiKalkulaator/veebiKalkulaator.js
+++ VeebiKalkulaator/veebiKalkulaator.js
@@ -1,51 +1,62 @@
+function Radio() {
+    const pildid = [
+        '../VeebiKalkulaator/images/eggs.webp',
+        '../VeebiKalkulaator/images/milk.webp',
+        '../VeebiKalkulaator/images/blackbread.jpg',
+        '../VeebiKalkulaator/images/bread.webp'
+    ];
+    let kanamunad = document.getElementById("kanamunad");
+    let piim = document.getElementById("piim");
+    let leib = document.getElementById("leib");
+    let sai = document.getElementById("sai");
+    let valitudToode = document.getElementById("valitudToode");
+
+    if (kanamunad.checked) {
+        valitudToode.src = pildid[0];
+    }
+    if (piim.checked) {
+        valitudToode.src = pildid[1];
+    }
+    if (leib.checked) {
+        valitudToode.src = pildid[2];
+    }
+    if (sai.checked) {
+        valitudToode.src = pildid[3];
+
+    }
+}
 function arvuta(kogus, hind){
-    return (kogus*hind).toFixed(2);
+    return(kogus*hind).toFixed(2);
     //toFixed - ümardab 2 märgini peale koma
 }
+
 const Hind1=2.5;
-const Hind2=5;
-const Hind3=1.8;
+const Hind2=1.8;
+const Hind3=2;
 const Hind4=2;
-const Hind5=2;
 
-function arvutaHind(){
-    let vastus2=document.getElementById('vastus2');
-    let tk=document.getElementById('tk');
-    let v1=document.getElementById('kanamunad');
-    let v2=document.getElementById('kana');
-    let v3=document.getElementById('piim');
-    let v4=document.getElementById('leib');
-    let v5=document.getElementById('sai');
+function arvutaPiltHind(){
+    let hindKokku=document.getElementById("hindKokku");
+    let number=document.getElementById("number");
+    let kanamunad=document.getElementById('kanamunad');
+    let piim=document.getElementById('piim');
+    let leib=document.getElementById('leib');
+    let sai=document.getElementById('sai');
+    //let button=document.getElementById("button");
 
-    if(v1.checked){
-        vastus2.innerHTML="kokku: "+ arvuta(tk.value, Hind1)+" euro";
-            vastus2.innerHTML="kodumaised kanamunad, otse Eesti taludest"
-
-        pilt.src="images/eggs.webp"
-
+    if(kanamunad.checked){
+        hindKokku.innerHTML=arvuta(number.value,Hind1)+" eurot";
     }
-    if(v2.checked){
-        vastus2.innerHTML="kokku: "+ arvuta(tk.value, Hind2)+" euro";
-        vastus2.innerHTML="kanad otse Eestist, või mdea"
-        pilt.src="images/chicken.webp"
-
+    if(piim.checked){
+        hindKokku.innerHTML=arvuta(number.value,Hind2)+" eurot";
     }
-    if(v3.checked){
-        vastus2.innerHTML="kokku: "+ arvuta(tk.value, Hind3)+" euro";
-        vastus2.innerHTML="Eesti piim, või midagi"
-        pilt.src="images/milk.webp"
-
+    if(leib.checked){
+        hindKokku.innerHTML=arvuta(number.value,Hind3)+" eurot";
     }
-    if(v4.checked){
-        vastus2.innerHTML="kokku: "+ arvuta(tk.value, Hind4)+" euro";
-        vastus2.innerHTML="Eesti must leib"
-        pilt.src="images/blackBread.jpg"
-
+    if(sai.checked){
+        hindKokku.innerHTML=arvuta(number.value,Hind4)+" eurot";
     }
-    if(v5.checked){
-        vastus2.innerHTML="kokku: "+ arvuta(tk.value, Hind5)+" euro";
-        vastus2.innerHTML="Eestis toodetud sai";
-        pilt.src="images/bread.webp"
 
-    }
+    hindKokku.style.backgroundColor="green";
+
 }
